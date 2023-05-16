c1489aa5cbd77fb64823385ed36f94aa6ef49e6c
Hrithik's <sandy.3ahirwar@gmail.com>
2023-05-16 22:04:55 +0530

    BinarySearch Added

---
+++ src/Search.java
@@ -0,0 +1,34 @@
+class Search {
+    public static int BinarySearch(int[] arr,int target){
+            //The Condition of Binary is The Array always remains Sorted
+            int left = 0 , right = arr.length-1;
+            int mid;
+            int count=0;
+            while(left <= right){
+                count++;
+                mid = ( left + right ) / 2;
+                if (arr[mid] == target){
+                    System.out.println("Occurance: "+count);
+                    return mid;
+                }
+                if (target > arr[mid])
+                    left = mid+1;
+                else
+                    right = mid-1;
+            }
+            System.out.println("Occurance: "+count);
+            return -1;
+        }
+}
+class UseSearch{
+    public static void main(String[] args) {
+
+        int[] arr;
+        arr=new int[50000000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i+1;
+        }
+        System.out.println(Search.BinarySearch(arr,500000));
+
+    }
+}
