ffcb50ab233ec2a97e893f6b61ff94040846b396
Hrithik's <sandy.3ahirwar@gmail.com>
2023-05-19 10:59:50 +0530

    insertionSort Implemented

--- src/Search.java
+++ src/Search.java
@@ -1,5 +1,10 @@
+import com.sun.security.jgss.GSSUtil;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 class Search {
-    public static int BinarySearch(int[] arr,int target){
+    public static final int binarySearch(int[] arr,int target){
             //The Condition of Binary is The Array always remains Sorted
             int left = 0 , right = arr.length-1;
             int mid;
@@ -9,7 +14,7 @@ class Search {
                 mid = ( left + right ) / 2;
                 if (arr[mid] == target){
                     System.out.println("Occurance: "+count);
-                    return mid;
+                    return arr[mid];
                 }
                 if (target > arr[mid])
                     left = mid+1;
@@ -18,17 +23,59 @@ class Search {
             }
             System.out.println("Occurance: "+count);
             return -1;
+    }
+}
+class Sorting{
+    public static final void bubbleSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length-1-i; j++) {
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+         }
+    }
+    public static final void selectionSort(int[] arr){
+        int min;
+        for (int i = 0; i < arr.length-1; i++) {
+            min = i;
+            for (int j = i+1 ; j < arr.length; j++) {
+                if (arr[j] < arr[min])
+                    min=j;
+            }
+            if (min!=i){
+                int temp=arr[i];
+                arr[i] = arr[min];
+                arr[min]=temp;
+            }
         }
+    }
+    public static final void insertionSort(int[] arr){
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j > 0; j--) {
+                if (arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1]=temp;
+                }
+                else
+                    break;
+            }
+        }
+    }
 }
 class UseSearch{
     public static void main(String[] args) {
 
-        int[] arr;
-        arr=new int[50000000];
+        int[] arr = new int[5000000];
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = i+1;
+            arr[i] = arr.length-i;
         }
-        System.out.println(Search.BinarySearch(arr,500000));
-
+        System.out.println(Arrays.toString(arr));
+        Sorting.insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Search.binarySearch(arr,45000));
     }
-}
+    }
